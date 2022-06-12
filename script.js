0cfdde6a910d9e13670b4e3e2c6e093b3167f522
elevin <jenya.levin@gmail.com>
2022-06-12 12:37:25 +1000

    Sunday 1

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var rowPos = 0;
 var cellPos = 0;
+var wordToGuess = 'HORSE'
+var enteredWord = []
+var rows
 
 function main() {
     rowPos = 0;
     cellPos = 0;
-    
+    enteredWord = [];
+
+    // Render HTML
     const main = document.querySelector(".main")
 
     for (let i = 0; i < 6; i++) {
@@ -21,20 +26,63 @@ function main() {
     }
 }
 
-main()
+function checkWord(enteredArray) {
+    const guessArray = Array.from(wordToGuess);
+    let greenCount = 0;
+    for (let i = 0; i < enteredArray.length; i++) {
+        let letterColor = '';
+        let letterPos = guessArray.indexOf(enteredArray[i])
+        if (letterPos === -1)
+            letterColor = 'grey';
+        else {
+            if (enteredArray[i] === guessArray[i]) {
+                letterColor = 'green';              
+                greenCount++;
+            }  
+            else {
+                letterColor = 'yellow';                
+            }
+        }
+        // Set correct colour on the cell
+
+        const currentCell = rows[rowPos].children[i];
+        currentCell.style.backgroundColor = letterColor;        
+    }
+
+    if (greenCount === 5) {
+        setTimeout(function() {
+            if (confirm("You Won!\nSelect OK to play again?")) {
+                main();
+            }
+            else {
+                return;
+            }
+        }, 100);
+    }
+
+    if (rowPos === 5)
+    {
+        setTimeout(function() {alert("You Lost!")}, 100);
+        return;
+    }
+}
 
 document.addEventListener('keyup', function(e) {
     const key = e.key;
+    if (rowPos == 6)
+        return;
+
     if (cellPos === 5 && key === 'Enter') {
         // Check word
+        checkWord(enteredWord);
+        enteredWord = [];
         rowPos++;
         cellPos = 0;
         return;
     }
     
-    if (rowPos < 6) {
-        rows = document.getElementsByClassName('row');
-    }
+    // Get cell (html) collection for the current row
+    rows = document.getElementsByClassName('row');
 
     if (cellPos > 0 && cellPos < 6 && key === 'Backspace') {
         cellPos--;
@@ -46,7 +94,10 @@ document.addEventListener('keyup', function(e) {
         // Populate next cell
         nextCell = rows[rowPos].children[cellPos];
         nextCell.textContent = key.toUpperCase();
+        enteredWord[cellPos] = key.toUpperCase();
         cellPos++
     }
-
+    
 });
+
+main()
